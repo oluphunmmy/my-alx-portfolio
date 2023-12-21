5abd3a65339ebdc271fd0b51480004ef1871b54b
oluphunmmy <olufunmilayoagboola@gmail.com>
2023-12-21 20:00:50 +0000

    webstack portfolio project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Designs";
+import Testimonials from "./components/Testimonials";
+import Designs from "./components/Designs";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Designs />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
